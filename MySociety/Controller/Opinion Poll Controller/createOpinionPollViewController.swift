6a5cea4b7a0d4e34af084c7e5625d0a791400374
shubham mishra <ramsmishra111@gmail.com>
2020-01-29 18:18:42 +0530

    opinion poll created successfully

--- MySociety/Controller/Opinion Poll Controller/createOpinionPollViewController.swift
+++ MySociety/Controller/Opinion Poll Controller/createOpinionPollViewController.swift
@@ -25,10 +25,76 @@ class createOpinionPollViewController: UIViewController {
     @IBOutlet weak var expDateAndTimePicker: UIDatePicker!
     @IBOutlet weak var CreatePollCreateBtn: UIButton!
     
+    @objc func CreateOpinionPollAction(sender:UIButton)
+    {
+        if subjectTextField.text ?? "" == ""{
+            showToast(message: "Type Subject for Poll", fontSize: 11.0)
+        }else if ATextField.text ?? "" == ""{
+            showToast(message: "Type Option A", fontSize: 11.0)
+        }else if BTextField.text ?? "" == ""{
+            showToast(message: "Type Option B", fontSize: 11.0)
+        }else if CTextField.text ?? "" == ""{
+            showToast(message: "Type Option C", fontSize: 11.0)
+        }else if DTextField.text ?? "" == ""{
+            showToast(message: "Type Option D", fontSize: 11.0)
+        }else{
+            self.postOpinionPoll()
+        }
+    }
+    
+    func postOpinionPoll(){
+        self.showSpinner(onView: self.view)
+        let parameters = [
+            "subject": "\(self.subjectTextField.text ?? "")",
+            "option_list": "\(ATextField.text ?? ""),\(BTextField.text ?? ""),\(CTextField.text ?? ""),\(DTextField.text ?? "")",
+            "user_id": "\(loggedInUserId)",
+            "end_time": "\(self.getDateInDateFormate(date: self.expDateAndTimePicker.date))"
+            ] as [String : Any]
+        let headerValues = ["x-api-key": "1c552e6f2a95a883209e9b449d6f4973", "Content-Type": "application/json"]
+        let request = getRequestUrlWithHeader(url: "addopenionpoll/\(loggedInUserId)", method: "POST", header: headerValues, bodyParams: parameters)
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            DispatchQueue.main.async {
+                self.removeSpinner()
+            }
+            if (error != nil) {
+                print(error ?? "")
+            } else {
+                let httpResponse = response as? HTTPURLResponse
+                
+                switch(httpResponse?.statusCode ?? 201){
+                case 200, 201:
+                    DispatchQueue.main.async {
+                        self.showAlertForError("Opinion Poll Created Successfully!!")
+                    }
+                default:
+                    DispatchQueue.main.async {
+                        self.showAlertForError("Some Error has occured, try again!")
+                    }
+                }
+            }
+        })
+        
+        dataTask.resume()
+    }
+        
+    func getDateInDateFormate(date: Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return formatter.string(from: date)
+    }
+    
+    func showAlertForError(_ message: String) -> (){
+        let alert = UIAlertController(title: message, message: nil , preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { _ in
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.CreatePollCreateBtn.addTarget(self, action: #selector(CreateOpinionPollAction(sender:)), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
